e0793b3ac6824203d2605e83bf4e0d1c41b58782
sofi <sofiaencina040704@gmail.com>
2024-11-28 15:29:14 -0300

    cambio funcion para numeros aleatorios del vector

--- include/Treesort.hpp
+++ include/Treesort.hpp
@@ -1,11 +1,14 @@
 #ifndef TREESORT_HPP
 #define TREESORT_HPP
 
+#include <random>
+
 #include <fstream>
 #include <cstdlib>
 #include <chrono>
 #include "Diccionario.hpp"
 #include "Vector.hpp"
+
 
 class Treesort{
     private:
@@ -36,7 +39,14 @@ Vector<int> Treesort::generar_vector_aleatorio(int tamanio){
     Vector<int> vector;
     int i = 0;
     while(i < tamanio){
-        int numero_alta = rand() % 10000;
+        // int numero_alta = rand() % 10000;
+        std::random_device rd; // Fuente de entropía
+        std::mt19937 gen(rd()); // Motor Mersenne Twister
+        std::uniform_int_distribution<> distrib(1, 10000); // Rango: [1, 10000]
+
+        // Generar un número aleatorio
+        int numero_alta = distrib(gen);
+        
         if(vector.ya_esta(numero_alta)){
         }else{
             vector.alta(numero_alta);
